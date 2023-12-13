e9f54704988bb7f67ff75a91b1273b66201f2f5b
Sharath kumar.B <sharathkumar044@gmail.com>
2023-12-13 17:40:00 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello SHARATH KUMAR YOU MADE IT!" );
+        System.out.println( "Hello SHARATH KUMAR YOU MADE IT! am checking my webhook" );
     }
 }
