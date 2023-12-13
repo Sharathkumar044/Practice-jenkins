5227b493fd14e6d551a59c51b13e7837cc9fe32b
Sharathkumar044 <sharathkumar044@gmail.com>
2023-12-13 11:02:24 +0530

    sample project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello SHARATH KUMAR YOU MADE IT!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello SHARATH KUMAR YOU MADE IT!" );
+    }
+}
